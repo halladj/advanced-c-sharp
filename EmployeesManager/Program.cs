75b63a5f4968527423f06c762d288f55e6901c13
halladj <halladj00@gmail.com>
2025-02-05 13:08:22 +0100

    added skiliton

---
+++ EmployeesManager/Program.cs
@@ -0,0 +1,52 @@
+using System.Net;
+using System.Text;
+using System.Text.Json;
+using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc.Routing;
+
+var builder = WebApplication.CreateBuilder(args);
+
+var app = builder.Build();
+
+app.MapGet("/", async ( HttpContext context) => {
+    //TODO: add support to arabic langauge via headers.
+
+    context.Response.Headers.Add("X-Custom-Header", "HeaderValue");
+
+    // Check if client wants JSON
+    if (context.Request.Headers.Accept.Any(h => h.Contains("application/json")))
+    {
+        context.Response.ContentType = "application/json";
+        await context.Response.WriteAsJsonAsync(new SpecialResponse{Key="key", Value="value"});
+    }
+    else
+    {
+        // Set HTML response
+        context.Response.ContentType = "text/html";
+        await context.Response.WriteAsync("""
+            <!DOCTYPE html>
+            <html>
+                <head>
+                    <title>Example Page</title>
+                </head>
+                <body>
+                    <h1>Hello from HTML!</h1>
+                </body>
+            </html>
+        """);
+    }
+});
+
+app.Run();
+
+
+
+
+
+public class SpecialResponse
+{
+    public string? Key { get; set; }  
+    public string? Value { get; set; } 
+    
+}
