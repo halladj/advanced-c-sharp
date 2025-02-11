80d3537d5a7f98126f2ccb39dacc6d441eb89d95
halladj <halladj00@gmail.com>
2025-02-11 23:07:52 +0100

    modified ReadME.md

--- EmployeesManager/obj/Debug/net9.0/EmployeesManager.AssemblyInfo.cs
+++ EmployeesManager/obj/Debug/net9.0/EmployeesManager.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EmployeesManager")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+852b744f1cf5b93b7d13aac35114d689448c1ab3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f0b0e0eee12b4ee1e059679fb7ebc5f099f88d3")]
 [assembly: System.Reflection.AssemblyProductAttribute("EmployeesManager")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EmployeesManager")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- README.md
+++ README.md
@@ -1,5 +1,11 @@
 # C#-Advances course, by Halladj.
-
+ 
+## Session-2
+    * introduce the Repository pattern.
+    * convert our data store to use Repository pattern.
+    * explore What are APIs in general, then dive into some detials about Minimal APIS, and OpenAPI.
+    * Add validation to our API using Data Anotations, and then learn about FluentValidation.
+    * Introduce the **controllers**, and convert our api to use them explicitly.
 
 
 
